713742a5e70c856fdf9482f2b0c348161a0543c3
tanayan299 <infuse130@gmail.com>
2023-04-01 03:02:42 +0900

    chore: yarn astro add image and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()]
+  integrations: [mdx(), tailwind(), image(), sitemap()]
 });
