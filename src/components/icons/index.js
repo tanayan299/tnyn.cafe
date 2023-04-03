68eeb748daa0ec6f87472f32b00fa701473c487c
tanayan299 <infuse130@gmail.com>
2023-04-03 16:11:35 +0900

    feat: update icons and details

---
+++ src/components/icons/index.js
@@ -0,0 +1,13 @@
+import IconSun from "./IconSun.astro"
+import IconGithub from "./IconGithub.astro"
+import IconTwitter from "./IconTwitter.astro"
+import IconArrowRight from "./IconArrowRight.astro"
+import IconMenu from "./IconMenu.astro"
+
+export {
+  IconSun,
+  IconGithub,
+  IconTwitter,
+  IconArrowRight,
+  IconMenu
+}
