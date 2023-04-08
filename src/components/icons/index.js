0e5ee910547b1dd799fc339962e0242587f98d19
tanayan299 <infuse130@gmail.com>
2023-04-09 00:03:24 +0900

    refactor: convert icons from handmade to astro-icon

--- src/components/icons/index.js
+++
@@ -1,8 +0,0 @@
-import IconSun from "./IconSun.astro";
-import IconGithub from "./IconGithub.astro";
-import IconTwitter from "./IconTwitter.astro";
-import IconArrowRight from "./IconArrowRight.astro";
-import IconMenu from "./IconMenu.astro";
-import IconRSS from "./IconRSS.astro";
-
-export { IconSun, IconGithub, IconTwitter, IconArrowRight, IconMenu, IconRSS };
