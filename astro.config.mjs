dc9841cf5914a3459e021f532da5a7f1ce0c21ce
tanayan299 <infuse130@gmail.com>
2023-04-09 08:21:31 +0900

    feat: add new configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,34 @@
 import path from "path";
 import { fileURLToPath } from "url";
-
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config.mjs";
-
+import partytown from "@astrojs/partytown";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    mdx(),
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    image(),
-    sitemap(),
-  ],
+  integrations: [mdx(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image(), sitemap(), partytown()],
   site: SITE.domain,
   base: SITE.baseUrl,
-
   output: "static",
-
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    sitemap(),
-    image(),
-  ],
-
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), sitemap(), image()],
   vite: {
     resolve: {
       alias: {
-        "~": path.resolve(__dirname, "./src"),
-      },
-    },
-  },
+        "~": path.resolve(__dirname, "./src")
+      }
+    }
+  }
 });
