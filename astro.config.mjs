8fa584ffc6e7130f3442a83ecb26f4d3dfbbfd2f
tanayan299 <infuse130@gmail.com>
2023-04-08 23:32:15 +0900

    refactor: add setting to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
+import * as url from "url";
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
-
 import { SITE } from "./src/config.mjs";
 
-// https://astro.build/config
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 export default defineConfig({
   integrations: [
     mdx(),
@@ -19,4 +20,25 @@ export default defineConfig({
     sitemap(),
   ],
   site: SITE.domain,
+  base: "/",
+
+  output: "static",
+
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    sitemap(),
+    image(),
+  ],
+
+  vite: {
+    resolve: {
+      alias: {
+        "~/": `${__dirname}src/`,
+      },
+    },
+  },
 });
