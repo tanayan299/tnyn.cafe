f628784188524c1320a0f3427fed42fde3dba642
tanayan299 <infuse130@gmail.com>
2023-04-09 11:19:30 +0900

    chore: yarn add @fontsource/inter

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
         secondary: colors.green,
       },
       fontFamily: {
-        sans: ["'Inter'", ...defaultTheme.fontFamily.sans],
+        sans: ["'InterVariable'", ...defaultTheme.fontFamily.sans],
       },
     },
   },
