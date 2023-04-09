ad9a15420f0af26ddd792de05bc85b36cca65ab7
tanayan299 <infuse130@gmail.com>
2023-04-09 16:26:38 +0900

    fix: fix GitHub Actions error

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 module.exports = {
+  // ...
   module: {
     rules: [
       {
         test: /\.node$/,
-        use: "node-loader",
+        loader: "node-loader",
+        options: {
+          // ネイティブモジュールのパスを指定
+          // この例では、pinyinパッケージを使用しています
+          name: "[name]-[hash].[ext]",
+          outputPath: "build/Release",
+          publicPath: "./build/Release/",
+        },
       },
     ],
   },
