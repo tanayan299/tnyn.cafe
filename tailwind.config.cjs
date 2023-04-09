98c1b2df46a3348a63b28e7e86f88d7a3ec1ad3b
tanayan299 <infuse130@gmail.com>
2023-04-09 23:33:14 +0900

    style: add setting of darkmode background color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,24 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
-        primary: 'var(--aw-color-primary)',
-        secondary: 'var(--aw-color-secondary)',
-        accent: 'var(--aw-color-accent)',
+        primary: "var(--aw-color-primary)",
+        secondary: "var(--aw-color-secondary)",
+        accent: "var(--aw-color-accent)",
       },
       fontFamily: {
-        sans: ['var(--aw-font-sans)', ...defaultTheme.fontFamily.sans],
-        serif: ['var(--aw-font-serif)', ...defaultTheme.fontFamily.serif],
-        heading: ['var(--aw-font-heading)', ...defaultTheme.fontFamily.sans],
+        sans: ["var(--aw-font-sans)", ...defaultTheme.fontFamily.sans],
+        serif: ["var(--aw-font-serif)", ...defaultTheme.fontFamily.serif],
+        heading: ["var(--aw-font-heading)", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundColor: {
+        dark: "#2e3545", // Dark Modeの背景色を変更
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
-  darkMode: 'class',
+  plugins: [require("@tailwindcss/typography")],
+  darkMode: "class",
 };
