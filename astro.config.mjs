ce02a6ddd9bc4d82b6c49c74b147429f313fe162
tanayan299 <infuse130@gmail.com>
2023-12-23 04:51:51 +0900

    refactor: transfer color code to RGB

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import { defineConfig } from 'astro/config';
+import { defineConfig, squooshImageService } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
@@ -73,5 +73,9 @@ export default defineConfig({
         '~': path.resolve(__dirname, './src'),
       },
     },
+  },
+
+  image: {
+    service: squooshImageService(),
   },
 });
