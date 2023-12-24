5c0e35d4f935476b91cff203e66f0569a5b75f7d
tanayan299 <infuse130@gmail.com>
2023-12-24 09:03:19 +0900

    fix: image related issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import { defineConfig, squooshImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
@@ -73,9 +73,5 @@ export default defineConfig({
         '~': path.resolve(__dirname, './src'),
       },
     },
-  },
-
-  image: {
-    service: squooshImageService(),
   },
 });
